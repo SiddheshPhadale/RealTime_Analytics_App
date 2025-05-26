ca132aa0f1397a594dca6f231a69655140aa1868
SiddheshPhadale <phadalesiddesh03@gmail.com>
2025-05-26 19:17:51 +0530

    Changes

--- analytics-consumer/src/main/java/com/analytics/analytics_consumer/configs/KafkaConfig.java
+++ analytics-consumer/src/main/java/com/analytics/analytics_consumer/configs/KafkaConfig.java
@@ -1,57 +1,55 @@
 package com.analytics.analytics_consumer.configs;
 
-import com.example.analytics.common.events.models.AddToCartEvent;
-import com.example.analytics.common.events.models.ProductViewEvent;
-import com.example.analytics.common.events.models.PurchaseEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
-import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.analytics.analytics_consumer.constants.Constants.groupId;
+
 
 @Configuration
 @EnableKafka
 public class KafkaConfig {
 
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
-    private ConsumerFactory<String, Object> factory(){
+    public ConsumerFactory<String, Object> consumerFactory(){
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.analytics.common.events.*");
 
-        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
 
-        Map<String, Class<?>> idMappings = new HashMap<>();
-        idMappings.put("productViewEvent", ProductViewEvent.class);
-        idMappings.put("addToCartEvent", AddToCartEvent.class);
-        idMappings.put("purchaseEvent", PurchaseEvent.class);
-        typeMapper.setIdClassMapping(idMappings);
-        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
+        props.put(JsonDeserializer.TYPE_MAPPINGS,
+                "productViewEvent:com.example.analytics.common.events.models.ProductViewEvent," +
+                        "addToCartEvent:com.example.analytics.common.events.models.AddToCartEvent," +
+                        "purchaseEvent:com.example.analytics.common.events.models.PurchaseEvent");
 
-        JsonDeserializer<Object> jsonDeserializer =new JsonDeserializer<>(Object.class);
-        jsonDeserializer.setTypeMapper(typeMapper);
-
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), jsonDeserializer);
+        return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(){
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(factory());
+        factory.setConsumerFactory(consumerFactory);
 
         return factory;
     }
